c7601dc8666949a2c9959b936b82c9ad2c67bd12
seva_ <sevagrbnv@gmail.com>
2023-03-30 13:34:23 +0300

    Initial commit

---
+++ djangoproject/test_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+from test_app.models import Worker
+
+
+def index_page(request):
+    # create
+    # new_worker = Worker(name='Иван', second_name='Иванов', salary=0)
+    # new_worker.save()
+
+    # get and update
+    # worker_by_id = Worker.objects.get(id=1)
+    # worker_by_id.salary = worker_by_id.salary + 10
+    # worker_by_id.save()
+
+    # get all
+    # all_workers = Worker.objects.all()
+    # print(all_workers)
+
+    # filter by ...
+    # workers_filtered = Worker.objects.filter(salary=0)
+    # print(workers_filtered)
+
+    # for i in all_workers:
+    #    print(f'{i.id} {i.second_name} {i.name} {i.salary}')
+
+    # get page
+    # return render(request, 'index.html')
+
+    workers = Worker.objects.all()
+
+    return render(request, 'index.html', context={'data': workers})
